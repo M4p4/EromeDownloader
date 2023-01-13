418c97e433ae9a88717e99fca4bd7da450764a97
M4p4 <homejr@protonmail.com>
2023-01-13 16:39:46 +0100

    already downloaded files are now skipped

--- dump.py
+++ dump.py
@@ -20,29 +20,37 @@ def collect_links(album_url):
 
     soup = BeautifulSoup(r.content, "html.parser")
     title = soup.find("meta", property="og:title")["content"]
-    print(title)
     videos = [video_source["src"] for video_source in soup.find_all("source")]
     images = [
         image["data-src"] for image in soup.find_all("img", {"class": "img-back"})
     ]
     urls = list(set([*videos, *images]))
     download_path = get_final_path(title)
+    existing_files = get_files_in_dir(download_path)
     for file_url in urls:
-        download(file_url, download_path, album_url)
+        download(file_url, download_path, album_url, existing_files)
 
 
 def get_final_path(title):
     final_path = os.path.join("downloads", title)
-    print(final_path)
     if not os.path.isdir(final_path):
         os.makedirs(final_path)
     return final_path
 
 
-def download(url, download_path, album=None):
-    print(f'[+] Downloading "{url}"')
+def get_files_in_dir(directory):
+    return [
+        f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))
+    ]
+
+
+def download(url, download_path, album=None, existing_files=[]):
     parsed_url = urlparse(url)
     file_name = os.path.basename(parsed_url.path)
+    if file_name in existing_files:
+        print(f'[#] Skipping "{url}" [already downloaded]')
+        return
+    print(f'[+] Downloading "{url}"')
     extracted = tldextract.extract(url)
     hostname = "{}.{}".format(extracted.domain, extracted.suffix)
     with session.get(
