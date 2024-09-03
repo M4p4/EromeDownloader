67709770e4bca7ae6186b512655e3eff998bf832
ryuvi <vicenters10@gmail.com>
2024-09-03 17:07:49 -0300

    added read links from file and color to text

--- dump.py
+++ dump.py
@@ -9,17 +9,24 @@ import tldextract
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 
+
 session = requests.Session()
+RED = "\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+RESET = "\033[0m"
+PURPLE = "\033[34m"
+PINK = "\033[35m"
 
 
 def collect_links(album_url: str) -> int:
     parsed_url = urlparse(album_url)
     if parsed_url.hostname != "www.erome.com":
-        raise Exception(f"Host must be www.erome.com")
+        raise Exception(f"{RED}Host must be www.erome.com{RESET}")
 
     r = session.get(album_url, headers={"User-Agent": "Mozilla/5.0"})
     if r.status_code != 200:
-        raise Exception(f"HTTP error {r.status_code}")
+        raise Exception(f"{RED}HTTP error {r.status_code}{RESET}")
 
     soup = BeautifulSoup(r.content, "html.parser")
     title = clean_title(soup.find("meta", property="og:title")["content"])
@@ -30,6 +37,7 @@ def collect_links(album_url: str) -> int:
     urls = list(set([*videos, *images]))
     download_path = get_final_path(title)
     existing_files = get_files_in_dir(download_path)
+    print(f"{PURPLE}[+] Downloading album '{title}'{RESET}")
     for file_url in urls:
         download(file_url, download_path, album_url, existing_files)
 
@@ -38,7 +46,7 @@ def collect_links(album_url: str) -> int:
 
 def clean_title(title: str) -> str:
     illegal_chars = r'[\\/:*?"<>|]'
-    return re.sub(illegal_chars, '_', title)
+    return re.sub(illegal_chars, "_", title)
 
 
 def get_final_path(title: str) -> str:
@@ -54,23 +62,25 @@ def get_files_in_dir(directory: str) -> list[str]:
     ]
 
 
-def download(url: str, download_path: str, album: str, existing_files: list[str]) -> None:
+def download(
+    url: str, download_path: str, album: str, existing_files: list[str]
+) -> None:
     parsed_url = urlparse(url)
     file_name = os.path.basename(parsed_url.path)
     if file_name in existing_files:
-        print(f'[#] Skipping "{url}" [already downloaded]')
+        print(f'{YELLOW}[#] Skipping "{url}" [already downloaded]{RESET}')
         return
-    print(f'[+] Downloading "{url}"')
+    print(f'{PINK}[+] Downloading "{url}"{RESET}')
     extracted = tldextract.extract(url)
     hostname = "{}.{}".format(extracted.domain, extracted.suffix)
     with session.get(
-            url,
-            headers={
-                "Referer": f"https://{hostname}" if album is None else album,
-                "Origin": f"https://{hostname}",
-                "User-Agent": "Mozila/5.0",
-            },
-            stream=True,
+        url,
+        headers={
+            "Referer": f"https://{hostname}" if album is None else album,
+            "Origin": f"https://{hostname}",
+            "User-Agent": "Mozila/5.0",
+        },
+        stream=True,
     ) as r:
         if r.ok:
             total_size_in_bytes = int(r.headers.get("content-length", 0))
@@ -82,13 +92,22 @@ def download(url: str, download_path: str, album: str, existing_files: list[str]
             progress_bar.close()
         else:
             print(r)
-            print(f'[ERROR] Download of  "{url}" failed')
+            print(f'{RED}[ERROR] Download of  "{url}" failed{RESET}')
             return None
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(sys.argv[1:])
-    parser.add_argument("-u", help="url to download", type=str, required=True)
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("-u", help="url to download", type=str)
+    group.add_argument("-f", help="file with urls to download", type=str)
     args = parser.parse_args()
-    files = collect_links(args.u)
-    print(f"[\u2713] Album with {files} files downloaded")
+    if args.u:
+        files = collect_links(args.u)
+        print(f"{GREEN}[\u2713] Album with {files} files downloaded{RESET}")
+    elif args.f:
+        with open(args.f, "r") as reader:
+            for line in reader.readlines():
+                if line.startswith("http"):
+                    files = collect_links(line.strip())
+                    print(f"{GREEN}[\u2713] Album with {files} files downloaded{RESET}")
