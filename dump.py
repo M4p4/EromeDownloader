8657c03404bb214fa940868f87868613c89c6a78
M4p4 <homejr@protonmail.com>
2024-09-03 19:19:39 +0200

    added progress bar when downloading check and clean title

--- dump.py
+++ dump.py
@@ -1,15 +1,18 @@
 import argparse
-import sys
-import requests
-from bs4 import BeautifulSoup
-from urllib.parse import urlparse
 import os
+import re
+import sys
+from urllib.parse import urlparse
+
+import requests
 import tldextract
+from bs4 import BeautifulSoup
+from tqdm import tqdm
 
 session = requests.Session()
 
 
-def collect_links(album_url):
+def collect_links(album_url: str) -> int:
     parsed_url = urlparse(album_url)
     if parsed_url.hostname != "www.erome.com":
         raise Exception(f"Host must be www.erome.com")
@@ -19,7 +22,7 @@ def collect_links(album_url):
         raise Exception(f"HTTP error {r.status_code}")
 
     soup = BeautifulSoup(r.content, "html.parser")
-    title = soup.find("meta", property="og:title")["content"]
+    title = clean_title(soup.find("meta", property="og:title")["content"])
     videos = [video_source["src"] for video_source in soup.find_all("source")]
     images = [
         image["data-src"] for image in soup.find_all("img", {"class": "img-back"})
@@ -30,21 +33,26 @@ def collect_links(album_url):
     for file_url in urls:
         download(file_url, download_path, album_url, existing_files)
 
+    return len(urls)
 
-def get_final_path(title):
+def clean_title(title: str) -> str:
+    illegal_chars = r'[\\/:*?"<>|]'
+    return re.sub(illegal_chars, '_', title)
+
+def get_final_path(title: str) -> str:
     final_path = os.path.join("downloads", title)
     if not os.path.isdir(final_path):
         os.makedirs(final_path)
     return final_path
 
 
-def get_files_in_dir(directory):
+def get_files_in_dir(directory: str) -> list[str]:
     return [
         f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))
     ]
 
 
-def download(url, download_path, album=None, existing_files=[]):
+def download(url: str, download_path: str, album: str | None, existing_files: list[str]) -> None:
     parsed_url = urlparse(url)
     file_name = os.path.basename(parsed_url.path)
     if file_name in existing_files:
@@ -54,18 +62,22 @@ def download(url, download_path, album=None, existing_files=[]):
     extracted = tldextract.extract(url)
     hostname = "{}.{}".format(extracted.domain, extracted.suffix)
     with session.get(
-        url,
-        headers={
-            "Referer": f"https://{hostname}" if album is None else album,
-            "Origin": f"https://{hostname}",
-            "User-Agent": "Mozila/5.0",
-        },
-        stream=True,
+            url,
+            headers={
+                "Referer": f"https://{hostname}" if album is None else album,
+                "Origin": f"https://{hostname}",
+                "User-Agent": "Mozila/5.0",
+            },
+            stream=True,
     ) as r:
         if r.ok:
+            total_size_in_bytes = int(r.headers.get("content-length", 0))
+            progress_bar = tqdm(total=total_size_in_bytes, unit="iB", unit_scale=True)
             with open(os.path.join(download_path, file_name), "wb") as f:
                 for chunk in r.iter_content(chunk_size=1024):
+                    progress_bar.update(len(chunk))
                     f.write(chunk)
+            progress_bar.close()
         else:
             print(r)
             print(f'[ERROR] Download of  "{url}" failed')
@@ -76,4 +88,5 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(sys.argv[1:])
     parser.add_argument("-u", help="url to download", type=str, required=True)
     args = parser.parse_args()
-    collect_links(args.u)
+    files = collect_links(args.u)
+    print(f"[\u2713] Album with {files} files downloaded")
